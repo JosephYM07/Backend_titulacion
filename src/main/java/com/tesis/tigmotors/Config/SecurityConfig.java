c25dea6eae8cae977c86bbd4ba2eacc5a91699ea
Joseph Yepez <joseph.yepez@puntonet.ec>
2024-11-15 15:46:30 -0500

    update

--- src/main/java/com/tesis/tigmotors/Config/SecurityConfig.java
+++ src/main/java/com/tesis/tigmotors/Config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configuración de CORS global
                 .authorizeHttpRequests(authRequest -> authRequest
-                        .requestMatchers("/api/v1/**").permitAll()
+                        .requestMatchers("/api/v1/**","/api/v1/password-reset-token/send-token","/api/v1/password/reset").permitAll()
                         .requestMatchers("/api/admin/**").hasAuthority("ADMIN")
                         .requestMatchers("/user/**","/api/v1/password-reset-token/").hasAuthority("USER")
                         .requestMatchers("/service-staff/**").hasAuthority("PERSONAL_CENTRO_DE_SERVICIOS")
