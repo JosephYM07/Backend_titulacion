c5d2ce2dc970c154dd6feb96aa7ecfedc60582ef
JosephYM07 <josepym07@gmail.com>
2024-12-01 10:43:24 -0500

    update cambio de contraseña en controladores

--- src/main/java/com/tesis/tigmotors/controller/AuthController.java
+++ src/main/java/com/tesis/tigmotors/controller/AuthController.java
@@ -2,47 +2,35 @@ package com.tesis.tigmotors.controller;
 
 import com.tesis.tigmotors.dto.Request.LoginRequest;
 import com.tesis.tigmotors.dto.Request.RegisterRequest;
+import com.tesis.tigmotors.dto.Request.ResetPasswordRequest;
 import com.tesis.tigmotors.dto.Response.AuthResponse;
-import com.tesis.tigmotors.service.AuthServiceImpl;
+import com.tesis.tigmotors.service.interfaces.AuthService;
+import com.tesis.tigmotors.service.interfaces.PasswordResetService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Controlador para manejar la autenticación y el restablecimiento de contraseñas.
+ */
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
 public class AuthController {
 
-    private final AuthServiceImpl authServiceImpl;
+    private final AuthService authService;
+    private final PasswordResetService passwordResetService;
 
-
-    /*@PostMapping("/login-users")
-    public ResponseEntity<AuthResponse> userLogin(@Valid @RequestBody LoginRequest request) {
-        if (request.getUsername() == null || request.getUsername().isEmpty() ||
-                request.getPassword() == null || request.getPassword().isEmpty()) {
-            throw new IllegalArgumentException("Se deben proporcionar tanto el nombre de usuario como la contraseña.");
-        }
-        log.info("Solicitud de inicio de sesión recibida para el usuario: " + request.getUsername());
-        return authService.loginAsUser(request);
-    }
-
-    // Endpoint de inicio de sesión para administradores
-    @PostMapping("/admin-login")
-    public ResponseEntity<AuthResponse> adminLogin(@Valid @RequestBody LoginRequest request) {
-        return authService.loginAsAdmin(request);
-    }
-
-    // Endpoint de inicio de sesión para el personal de centro de servicios
-    @PostMapping("/service-staff-login")
-    public ResponseEntity<AuthResponse> serviceStaffLogin(@Valid @RequestBody LoginRequest request) {
-        return authService.loginAsServiceStaff(request);
-    }*/
     @PostMapping("/login-global")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request) {
         if (request.getUsername() == null || request.getUsername().isEmpty() ||
@@ -50,10 +38,9 @@ public class AuthController {
             throw new IllegalArgumentException("Se deben proporcionar tanto el nombre de usuario como la contraseña.");
         }
         log.info("Solicitud de inicio de sesión recibida para el usuario: " + request.getUsername());
-        return authServiceImpl.login(request);
+        return authService.login(request);
     }
 
-    //Endpoint para registrar solo usuarios
     @PostMapping("/register-user")
     public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest request) {
         try {
@@ -67,7 +54,45 @@ public class AuthController {
             return ResponseEntity.badRequest().build();
         }
         log.info("Solicitud de registro recibida para el usuario: " + request.getUsername());
-        return authServiceImpl.register(request);
+        return authService.register(request);
     }
 
+    @PostMapping("/send-token")
+    public String sendResetToken(@RequestParam String email) {
+        log.info("Solicitud de envío de código de recuperación para el correo: {}", email);
+        try {
+            String response = passwordResetService.sendResetToken(email);
+            log.info("Código de recuperación enviado exitosamente a: {}", email);
+            return response;
+        } catch (Exception e) {
+            log.error("Error al enviar el código de recuperación al correo: {}. Error: {}", email, e.getMessage());
+            throw new RuntimeException("Error al enviar el código de recuperación", e);
+        }
+    }
+
+    @PostMapping("/password/reset")
+    public ResponseEntity<Map<String, String>> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPasswordRequest) {
+        log.info("Solicitud de restablecimiento de contraseña recibida");
+        log.debug("Datos recibidos - Token: {}, Nueva Contraseña: [PROTEGIDO]", resetPasswordRequest.getToken());
+
+        Map<String, String> response = new HashMap<>();
+        try {
+            String resultMessage = passwordResetService.resetPassword(resetPasswordRequest.getToken(), resetPasswordRequest.getNewPassword());
+            response.put("message", resultMessage);
+            log.info("Contraseña restablecida exitosamente para el token: {}", resetPasswordRequest.getToken());
+            return ResponseEntity.ok(response);
+        } catch (RuntimeException e) {
+            log.error("Error al restablecer la contraseña: {}", e.getMessage(), e);
+            response.put("error", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+        }
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleResponseStatusException(ResponseStatusException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("error", ex.getReason());
+        log.error("Excepción manejada: {}", ex.getReason());
+        return ResponseEntity.status(ex.getStatusCode()).body(errorResponse);
+    }
 }
