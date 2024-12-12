60a77d6dc95ce6b8b3e7c95a269a4df95182616a
JosephYM07 <josepym07@gmail.com>
2024-12-12 11:21:45 -0500

    Update application.properties

--- src/main/java/com/tesis/tigmotors/TigMotorsApplication.java
+++ src/main/java/com/tesis/tigmotors/TigMotorsApplication.java
@@ -9,17 +9,6 @@ public class TigMotorsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TigMotorsApplication.class, args);
 	}
+
 }
-/*@Configuration
-public static class Myconfiguration{
-	@Bean
-	public WebMvcConfigurer corsConfigurer(){
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-			}
-		};
-	}
-}*/
+
