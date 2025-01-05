90557a52d3a077e73006bb3de5f2086c03a9028f
JosephYM07 <josepym07@gmail.com>
2025-01-05 15:44:18 -0500

    Update public

--- src/main/java/com/tesis/tigmotors/controller/PublicController.java
+++ src/main/java/com/tesis/tigmotors/controller/PublicController.java
@@ -13,20 +13,8 @@ public class PublicController {
 
     @GetMapping("/status")
     public ResponseEntity<String> getStatus() {
-        String saludo = generarSaludo();
-        String mensaje = saludo + " El sistema está funcionando correctamente! "
+        String mensaje = " El sistema está funcionando correctamente! "
                 + "Consulta la documentación en: https://documenter.getpostman.com/view/34383022/2sAYHwKQaJ";
         return ResponseEntity.ok(mensaje);
-    }
-
-    private String generarSaludo() {
-        LocalTime ahora = LocalTime.now();
-        if (ahora.isBefore(LocalTime.NOON)) {
-            return "¡Buenos días!";
-        } else if (ahora.isBefore(LocalTime.of(18, 0))) {
-            return "¡Buenas tardes!";
-        } else {
-            return "¡Buenas noches!";
-        }
     }
 }
